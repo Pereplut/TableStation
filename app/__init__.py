8d5f4f2f523f636f6f481db695e36d7cc89b222a
Pereplut <d.guyvan@gmail.com>
2016-11-11 12:07:02 +0100

    added config file

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,13 @@ from sqlalchemy.orm import sessionmaker
 
 from app.models import LightTimeStamp, TemperatureTimeStamp, Base
 
-engine = create_engine('sqlite:///tableStation.db')
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
 app = Flask(__name__)
+app.config.from_object('config')
+db=create_engine(app)
+#engine = create_engine('sqlite:///tableStation.db')
+Base.metadata.bind = db
+DBSession = sessionmaker(bind=db)
+session = DBSession()
+
 
 from app import models,view
