b4715a675992e9f3e08f89365e2d167f99d38c4b
Pereplut <d.guyvan@gmail.com>
2016-12-08 23:40:00 +0100

    monitor.py with logging

---
+++ MonitorArduino.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.4
+import logging
+import monitor
+# create logger with 'spam_application'
+logger = logging.getLogger('Arduino_app')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('serial_monitor.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+logger.info('launching serial Monitoring')
+monitoring = monitor.ArduinoSerial(port='/dev/ttyACM0')
+#serialCom = monitoring(port='/dev/ttyACM0')
+logger.info('launched an instance of serial Monitoring')
+logger.info('starting monitoring loop')
+monitor.readFrom_Serial(monitoring)
+logger.info('ending monitoring loop')
--- monitor.py
+++ monitor.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3.4
 import time
+import logging
 import serial
 from sqlalchemy import create_engine
 from sqlalchemy.orm import  sessionmaker
 from app.models import LightTimeStamp, TemperatureTimeStamp, Base
 
+module_logger = logging.getLogger('app.mon_While')
 engine = create_engine('sqlite:///tableStation.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
@@ -15,6 +17,10 @@ class ArduinoSerial():
                  parity=serial.PARITY_NONE,timeout=5):
         self.ser=serial.Serial(port=port,baudrate=baudrate,bytesize=bytesize,
                                stopbits=stopbits,parity=parity,timeout=timeout)
+        self.logger = logging.getLogger('app.mon_While.ArduinoSerial')
+        self.logger.info('creating an instance of ArduinoSerial')
+
+
     def open(self):
         try:
             self.ser.open()
@@ -26,31 +32,37 @@ class ArduinoSerial():
         self.ser.close()
 
     def send(self, message):
+        self.logger.info('sent data')
         messageThis=message.encode('utf-8')
         self.ser.write(messageThis)
 
+
     def recive(self):
+        self.logger.info('receiving...')
         self.ser.flush()
         return self.ser.readline().decode('utf-8')
 
-serialCom = ArduinoSerial(port='/dev/ttyACM0')
+#serialCom = ArduinoSerial(port='/dev/ttyACM0')
 
 def readFrom_Serial(serial_instance):
     while True:
         #serial_instance.flushInput()
         serial_instance.send("1")
         responce = serial_instance.recive()
+        module_logger.info('received data from Arduino, %s')
         responce.strip(' \t\n\r')
         #print (responce)
         if responce != '':
             temperature, light = responce.split(',')
+            module_logger.info('sending data to DB')
             Temperature = TemperatureTimeStamp(temper_inC_Value=temperature)
             session.add(Temperature)
             Light = LightTimeStamp(lightValue=light)
             session.add(Light)
             session.commit()
+            module_logger.info('commit has been performed')
         time.sleep(60)
     serial_instance.close()
 
-readFrom_Serial(serialCom)
+#readFrom_Serial(serialCom)
 
